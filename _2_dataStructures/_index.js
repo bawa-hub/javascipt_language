2bf740754f1f7d1140222d1254b86f29316ce8d8
bawa-hub <itsworkingmind@gmail.com>
2022-03-07 21:55:10 +0530

    initial commit

---
+++ _2_dataStructures/_index.js
@@ -0,0 +1,9 @@
+// In JavaScript, a variable may store two types of values: primitive and reference
+// JavaScript provides six primitive types as
+// undefined, null, boolean, number, string, and symbol ,
+
+// and a reference type object.
+// The size of a primitive value is fixed, therefore, JavaScript stores the primitive value on the stack.
+// On the other hand, the size of a reference value is dynamic so JavaScript stores the reference value on the heap
+
+// https://www.javascripttutorial.net/javascript-primitive-vs-reference-values/ for more
---
+++ _5_typescript/main.ts
@@ -0,0 +1,6 @@
+function add(x: number, y: number) {
+  return x + y;
+}
+
+let result = add(1, 2);
+console.log(result);
