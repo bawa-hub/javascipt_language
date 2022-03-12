804c0559497b099f824bb64d857739f30c81a6ea
bawa-hub <itsworkingmind@gmail.com>
2022-03-13 00:41:43 +0530

    folder structure update

--- _2_dataStructures/_index.js
+++ _2_dataStructures/_index.js
@@ -1,6 +1,6 @@
 // In JavaScript, a variable may store two types of values: primitive and reference
-// JavaScript provides six primitive types as
-// undefined, null, boolean, number, string, and symbol ,
+// JavaScript provides seven primitive types as
+// undefined, null, boolean, number,BigInt string, and symbol ,
 
 // and a reference type object.
 // The size of a primitive value is fixed, therefore, JavaScript stores the primitive value on the stack.
