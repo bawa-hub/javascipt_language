ef3d8d4a221de526f5984dc2742a3e24e6b9edb0
bawa-hub <itsworkingmind@gmail.com>
2022-03-13 10:48:14 +0530

    frontendmaster hardparts pdf added

--- _1_basics/_1_statements_and_declarations/_4_iterations/index.js
+++ _1_basics/_1_statements_and_declarations/_4_iterations/index.js
@@ -1,4 +1,5 @@
-//  for statement creates a loop that consists of three optional expressions, enclosed in parentheses and separated by semicolons, followed by a statement (usually a block statement) to be executed in the loop.
+//  for statement creates a loop that consists of three optional expressions,
+// enclosed in parentheses and separated by semicolons, followed by a statement (usually a block statement) to be executed in the loop.
 let str = "";
 for (let i = 0; i < 9; i++) {
   str = str + i;
