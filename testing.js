ea4b46b8f11a30db5b0fc208b55e4e947b33f005
bawa-hub <itsworkingmind@gmail.com>
2022-03-20 22:05:02 +0530

    updated

--- testing.js
+++ testing.js
@@ -1,2 +1,10 @@
-const os = require("os");
-console.log(os.arch());
+// const os = require("os");
+// console.log(os.arch());
+
+function printHello() {
+  console.log("Hello");
+}
+function blockFor1Sec() {}
+setTimeout(printHello, 0);
+blockFor1Sec();
+console.log("Me first!");
